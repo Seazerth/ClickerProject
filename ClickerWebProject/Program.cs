ccf7375b4e179ed19ff3ca421c57ba39bdd77f79
Seazerth <timmark2302@gmail.com>
2024-09-25 22:06:46 +0500

    add folders

--- ClickerWebProject/Program.cs
+++ ClickerWebProject/Program.cs
@@ -5,9 +5,12 @@ namespace ClickerWebProject
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            
+            builder.Services.AddHealthChecks();
             var app = builder.Build();
 
             app.MapGet("/", () => "Hello World!");
+            app.MapHealthChecks("health-check");
 
             app.Run();
         }
